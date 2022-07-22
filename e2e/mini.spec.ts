5f6261bf11f53520a231e9bf53f95bb595e34b53
NgoVanDat <dat.ngo@elpis.game>
2022-07-22 11:46:46 +0700

    Add unit test

--- e2e/mini.spec.ts
+++ e2e/mini.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
+import {styleCombine} from "@Common/helper";
 
 test('should be logged in successfully', async ({page}) => {
-  await page.goto('http://localhost:3003/');
+  await page.goto('http://localhost:3000/');
   await page.locator('#email').fill('test@gmail.com');
   await page.locator('#password').fill('test');
   await page.click('text=Login / Register');
@@ -11,22 +12,26 @@ test('should be logged in successfully', async ({page}) => {
 })
 
 test('not logged in', async ({page}) => {
-  await page.goto('http://localhost:3003/');
+  await page.goto('http://localhost:3000/');
   await expect(page.locator('#login_register')).toContainText('Login / Register');
 })
 
 test('should navigate to the share page', async ({ page }) => {
-  await page.goto('http://localhost:3003/');
+  await page.goto('http://localhost:3000/');
   await page.locator('#email').fill('test@gmail.com');
   await page.locator('#password').fill('test');
   await page.click('text=Login / Register');
   await page.click('text=Share a movie');
-  await expect(page).toHaveURL('http://localhost:3003/share');
+  await expect(page).toHaveURL('http://localhost:3000/share');
   await expect(page.locator('#share')).toContainText('Share');
 })
 
 test('Not logged in so can`t access the share page', async ({ page }) => {
-  await page.goto('http://localhost:3003/share');
-  await expect(page).toHaveURL('http://localhost:3003');
+  await page.goto('http://localhost:3000/share');
+  await expect(page).toHaveURL('http://localhost:3000');
   await expect(page.locator('#login_register')).toContainText('Login / Register');
 })
+
+test('styleCombine function', () => {
+  expect(styleCombine('class1', 'class2')).toBe(('class1 class2'));
+});
